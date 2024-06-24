914d67263530df160745a72c3e64762deab0a46b
MickeyD <cameron.mcdaniel10@gmail.com>
2024-06-24 19:13:56 -0400

    DC17SA code line updated
    Updated a line for the DC17SA to reflect the new classname

--- 315th-Weapons/DC-17SA/config.cpp
+++ 315th-Weapons/DC-17SA/config.cpp
@@ -17,7 +17,7 @@ class CfgWeapons
 	class 315_DC17SA: JLTS_DC17SA
 	{
 		displayName="[315th] DC-17SA";
-		baseweapon="Aux315_DC17SA";
+		baseweapon="315_DC17SA";
 		cursor="DC_Series_Reticle";
 		scope=2;
 		scopeArsenal=2;
